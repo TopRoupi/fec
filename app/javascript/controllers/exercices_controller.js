425297e979df59f32572528dae86cbaeaabec295
Breno <breno.nunesgalvao@protonmail.ch>
2020-02-22 09:12:44 -0300

    async judge0 requests on exercices_controller.js

--- app/javascript/controllers/exercices_controller.js
+++ app/javascript/controllers/exercices_controller.js
@@ -5,12 +5,30 @@ var editor;
 export default class extends Controller {
   static targets = [ "lang", "code", 'mode' ]
 
-  generate_test_output(event) {
-    var input = event.currentTarget.parentNode.children[0].children[1].value
-    var output = event.currentTarget.parentNode.children[1].children[1]
-    var lang = document.getElementById(`lang${this.langTarget.value}`).innerHTML
-    var code = editor.getValue()
+  result(token, output) {
+    $.ajax({
+      url: "https://api.judge0.com/submissions/" + token,
+      context: this,
+      type: "GET",
+      success: function (data) {
+        if (data.status.id <= 2) { // In Queue or Processing
+          setTimeout(() => {
+            this.result(token, output)
+          }, 250)
+          return
+        }
 
+        if(data.stdout){
+          output.value = data.stdout
+        }
+        else {
+          output.value = `ERROR: ${data.stderr}`
+        }
+      }
+    })
+  }
+
+  token(lang, code, input, output) {
     var to_compile = {
       "language_id": lang,
       "source_code": code,
@@ -20,32 +38,22 @@ export default class extends Controller {
     $.ajax ({
       url: "https://api.judge0.com/submissions",
       type: "POST",
+      context: this,
       data: to_compile
     }).done(function(data) {
-
-      do {
-        var res
-        $.ajax({
-          url: `https://api.judge0.com/submissions/${data.token}`,
-          type: "GET",
-          async: false
-        }).done(function(data) {
-          res = data
-        })
-      } while (res.status.id <= 2);
-
-      if(res.stdout){
-        output.value = res.stdout
-      }
-      else {
-        output.value = `Error: ${res.stderr}`
-      }
-
+      this.result(data.token, output)
     }).fail(function(data, err) {
-
       alert("fail " + JSON.stringify(data) + " " + JSON.stringify(err))
-
     })
+  }
+
+  run(event) {
+    var input = event.currentTarget.parentNode.children[0].children[1].value
+    var output = event.currentTarget.parentNode.children[1].children[1]
+    var lang = document.getElementById(`lang${this.langTarget.value}`).innerHTML
+    var code = editor.getValue()
+
+    this.token(lang, code, input, output)
   }
 
   connect() {
