c401ce73488e474f6dc44de27b5fd6f6724b0c0f
Breno <breno.nunesgalvao@protonmail.ch>
2020-08-29 07:19:59 -0300

    prefetch links

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,8 @@
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
+import { PrefetchTurbolinksController } from '@hopsoft/controllers'
 
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
+application.register('prefetch', PrefetchTurbolinksController)
