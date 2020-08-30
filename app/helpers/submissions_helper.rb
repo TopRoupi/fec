87f17856f227e3ff38d6b760accde3c8ee1fd8e0
Breno <breno.nunesgalvao@protonmail.ch>
2020-08-30 20:01:25 -0300

    profile and notifications navbar dropdown

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,6 +1,2 @@
 module SubmissionsHelper
-  def render_submission(submission)
-    @submission = submission
-    render 'submissions/submission_results'
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   has_many :submissions
   has_one :do_later_list, class_name: 'List', foreign_key: :owner_id
-	enum role: [:user, :admin]
-	after_initialize :set_default_role, :if => :new_record?
+  enum role: [:user, :admin]
+  after_initialize :set_default_role, if: :new_record?
+  after_initialize :set_do_later_list, if: :new_record?
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -18,6 +19,15 @@ class User < ApplicationRecord
   end
 
   private
+
+  def set_do_later_list
+    self.do_later_list = List.new(
+      name: 'Do later list',
+      description: 'Procastination list',
+      privacy: :private_list,
+      owner: self
+    )
+  end
 
   def set_default_role
     self.role ||= :user
